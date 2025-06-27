a251672145914c5962ab3afce3fed9d3fa125340
Marethy <khiemmm2004@gmail.com>
2025-06-27 20:38:51 +0700

    feat: configure middleware

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+
+const isProtectedRoute = createRouteMatcher(['/']);
+
+export default clerkMiddleware(async (auth, request) => {
+  if (isProtectedRoute(request)) {
+    await auth().protect();
+  }
+
+  return NextResponse.next();
+});
+
+export const config = {
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
